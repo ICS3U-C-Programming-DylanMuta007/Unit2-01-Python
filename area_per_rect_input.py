a9b3242584f017804d59e024721cc564a8b1937e
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-02-23 18:01:20 +0000

    Added

---
+++ area_per_rect_input.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created by Dylan Mutabazi
+# Date :23-02-2025
+# This program calculates the perimeter & area of a rectangle
+
+import math
+
+print("")
+print("what is the width of your rectangle")
+width = float(input(""))
+
+print("")
+print("what is the length of your rectangle")
+length = float(input(""))
+
+print("")
+print("The Area of your rectangle = {}".format(width * length))
+print("The Perimeter of your rectangle = {}".format(2 * (width + length)))
